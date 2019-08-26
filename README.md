5629ae60f9a41c52600d6c2749742843976f1bd3
Matteo Esposito <actumatteo@gmail.com>
2019-08-26 15:40:51 -0400

    working

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# moodler
-PDF Downloader for Concordia Moodle
+# Moodler
+
+App that takes in any Concordia student ID, password and moodle course to then download all pdf and pptx files on that moodle page.
+
+<p align="center">
+  <img src="/assets/2.png" height="505" width="759">
+</p>
+
+## Requirements
+
+#### Firefox
+
+[Install here](https://www.mozilla.org/en-CA/firefox/new/)
+
+#### Geckodriver
+You will need to install geckodriver for access to the "HTTP API described by the WebDriver protocol to communicate with Gecko browsers, such as Firefox." 
+
+Having the Homebrew package manager will make things easier. Install by running the following (Mac):
+
+```bash
+$ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+$ brew install geckodriver
+```
+
+or via this link (Windows/Linux):
+
+|OS|Link|
+|--|--|
+|Windows 32 bit| [mozilla/geckodriver/win32.zip](https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-win32.zip)|
+|Windows 64 bit| [mozilla/geckodriver/win64.zip](https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-win64.zip)|
+|Linux 32 bit| [mozilla/geckodriver/linux32.zip](https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux32.tar.gz)|
+|Linux 64 bit| [mozilla/geckodriver/linux64.zip](https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz)|
+
+## Usage
+
+Clone the repository and run the app.
+```bash
+$ git clone https://github.com/matteo-esposito/moodler.git
+$ cd moodler
+$ python app.py
+```
+
+Enter Concordia username, password, moodle course link and output folder path when prompted. (Password is hidden during input.)
+
+<p align="center">
+  <img src="/assets/1.png" height="505" width="759">
+</p>
+
+Download files.
+
+<p align="center">
+  <img src="/assets/2.png" height="505" width="759">
+</p>
--- app.py
+++ app.py
@@ -10,21 +10,36 @@ import requests
 import sys
 
 moodle_page = 'https://moodle.concordia.ca/moodle/login/index.php'
-course_page = 'https://moodle.concordia.ca/moodle/course/view.php?id=112046' # COMP352
 
 class Moodler:
-    def __init__(self, username, password):
+    def __init__(self, username, password, course_page, outpath):
         """Moodler. Your automated Moodle pdf downloader.
 
         Arguments:
-            username {string} -- Moodle username
-            password {string} -- Moodle password
+            username {string} -- Moodle username.
+            password {string} -- Moodle password.
+            course_page {string} -- Moodle course page where files are.
+            outpath {string} -- Destination folder.
         """
         self.username = username
         self.password = password
+        self.course_page = course_page
+        self.outpath = outpath
+        
+        mime_types = "application/pdf,application/vnd.adobe.xfdf,application/vnd.fdf,application/vnd.adobe.xdp+xml"
+
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("browser.download.folderList", 2)
+        fp.set_preference("browser.download.manager.showWhenStarting", False)
+        fp.set_preference("browser.download.dir", self.outpath)
+        fp.set_preference("browser.helperApps.neverAsk.saveToDisk", mime_types)
+        fp.set_preference("plugin.disable_full_page_plugin_for_types", mime_types)
+        fp.set_preference("pdfjs.disabled", True)
+
         opts = webdriver.FirefoxOptions()
-        opts.headless = False
-        self.bot = webdriver.Firefox(options=opts)
+        opts.headless = True
+        
+        self.bot = webdriver.Firefox(options=opts, firefox_profile=fp)
 
     def login(self):
         """Uses username and password to login to the moodle.
@@ -54,51 +69,60 @@ class Moodler:
         print("✓ Logged in successfully.")
 
     def goto_class(self):
+        """Get to desired class page.
+        """
         bot = self.bot
-        bot.find_element_by_xpath('//a[@href="' + course_page + '"]').click()
+        bot.find_element_by_xpath('//a[@href="' + self.course_page + '"]').click()
         time.sleep(3.5)
         print("✓ Got to course page.")
 
-    def get_pdflist(self):
+    def get_docs(self):
+        """Download files from moodle page.
+        """
         bot = self.bot
 
         # Scrape
         soup = BeautifulSoup(bot.page_source, features="lxml")
         divs = soup.findAll('div', attrs={'class' : 'activityinstance'})
+        pdf_icon = 'https://moodle.concordia.ca/moodle/theme/image.php/clean/core/1565843043/f/pdf-24'
         pdf_links = []
         for div in divs:
-            pdf_links.append(div.find('a')['href'])
+            href = div.find('a')['href']
+            if div.find('img')['src'] == pdf_icon:
+                pdf_links.append(href)
 
         # Keep pdf links
         cleaned_links = [x for x in pdf_links if x[:48] == 'https://moodle.concordia.ca/moodle/mod/resource/']
 
-        # Instead of just visiting the known links, click on the pdfs...annoying but the only way to get through Moodle's login refreshing -_-
-        for i, link in enumerate(cleaned_links[:1]):
-            outpath = folder_location + 'file_{}.pdf'.format(i)
+        all_files = []
+        new_files = []
+        for i, link in enumerate(cleaned_links[:5], 1):
+            all_files = os.listdir(self.outpath) # Before
             bot.find_element_by_xpath('//a[@href="' + link + '"]').click()
-            time.sleep(1.5)        
-            temp = bot.find_element_by_xpath(
-            '//input[@class="toolbarButton" and @id="download"]').click()
-            time.sleep(2)
-            temp.send_keys(Keys.RETURN)
-            bot.back()
+            time.sleep(1)
+            new_files = os.listdir(self.outpath) # After
+            most_recent_file = list(set(new_files) - set(all_files))
+            print('{}. {} saved.'.format(i, most_recent_file[0]))
 
         print("✓ Saved all available files.")
 
 if __name__ == '__main__':
     
-    # Accept user input for username, password and semester
+    # Accept user input for username, password and file output path
     user = input('Username: ')
     pwd = getpass.getpass()
-    moodler = Moodler(user, pwd)
+    cse_pg = input('Course page: ')
+    outpath = input('Destination folder: ')
 
-    # Login
+    # Create destination folder in the case where it doesn't exist.
+    if not os.path.exists(outpath):
+        os.mkdir(outpath)
+
+    moodler = Moodler(user, pwd, cse_pg, outpath)
+
+    # Login & get to class page
     moodler.login()
     moodler.goto_class()
 
-    # Create destination folder in the case where it doesn't exist.
-    folder_location = '/Users/Matteo/Desktop/pdfs/'
-    if not os.path.exists(folder_location):
-        os.mkdir(folder_location)
-
-    # print(moodler.get_pdflist())
+    # Download files
+    moodler.get_docs()
